d1ba89febbe07f58d44d613056a1ffdd10223c96
JJoeNapoli <45485426+JJoeNapoli@users.noreply.github.com>
2019-07-07 12:55:32 +0200

    istruzioni per mrkrs mancanti

--- MATLAB/code/3 version/main_rb.m
+++ MATLAB/code/3 version/main_rb.m
@@ -15,7 +15,7 @@ bag_name="../../bag_file/mano_rb.bag";        %qualche mrks in più ma mai in men
 % bag = rosbag("../../bag_file/2019-07-01-18-21-37.bag");
 % bag = rosbag("../../bag_file/2019-07-01-18-30-15.bag");
 
-%% clean fromm bad data
+%% clean from bad data
 V=clean_noises(V,RB);
 
 %% transformation matrix for each msg
@@ -28,14 +28,13 @@ for I=1:num_msgs
     [oRm,ind]=set_orient(V(I,1).field,RB(I,1).field);
     [V,RB]=move_nonimu(V,RB,I,ind);
     
-    
     %% set oTm and mTo
     
     oTm(:,:,I)=[   oRm,        RB(I,1).field';
         zeros(1,3),     1];
     mTo(:,:,I)=[   oRm^-1,       (-oRm^-1)*RB(I,1).field';
         zeros(1,3),     1];
-%     oTm(:,:,I)*mTo(:,:,I) %=eye(4)
+    %     oTm(:,:,I)*mTo(:,:,I) %=eye(4)
     %% homogeneous coordiantes
     
     V(I,1).field=[V(I,1).field,ones(size(V(I,1).field,1),1)];
@@ -55,12 +54,32 @@ mVdes=set_Vdes(V,mTo(:,:,1));
 % sempre nel frame relativo relativo
 A=findA(V(1,1).field,mVdes,mTo(:,:,1));
 
+%%  check if some data are lost and sort them
 
-%% W
+num_mrks=size(V(I,1).field);
+for I=1:num_msgs
+    
+    if num_mrks == size(V(I,1).field)
+        %% W
+        W(:,:,I) = A * V(1,1).field;
+        
+    else
+        old = A' * W(:,:,I-1);
+        mlast_pos = my_transform(old,mTo);
+        % vado a vedere le diff dei vettori ripetto a m, il primo
+        % più grande è la posizione (ind) di quello scomparso,
+        % bisogna farlo per più volte, finché non si raggiunge il numero di
+        % markrs iniziale
+        % trovato quello scomparso gli si da la stessa posizione relativa
+        % di prima
+        % usiamo W  e lo disordiniamo perché almeno abbiamo la posizione
+        % precedente già aggiustata (se usassimo V e se in due msgs 
+        % consecutivi mancasse un mrk sarebbe merda)
+    end
+end
 
-W = A * V(1,1).field;
 
-%% graph
+% graph
 % plot the cleaned data
 
 % figure(),hold on
@@ -68,31 +87,24 @@ W = A * V(1,1).field;
 % plot3(V(1,1).field(:,1),V(1,1).field(:,2),V(1,1).field(:,3),'or')
 % plot3(RB(1,1).field(1,1),RB(1,1).field(1,2),RB(1,1).field(1,3),'ob'),
 % grid on
-% 
+%
 % my_plot(V(1,1).field);
 % my_plot(W);
-% 
+%
 % my_plot(mVdes)
 
 
-%% load knucles 
+%% load knucles
 load("knucles.mat")
-
 
 %% compute the following knucles
 nocche=my_transform(mnocche,oTm(:,:,1));
 % my_plot([W;nocche]);
-% 
+%
 % mV=my_transform(V(1,1).field,mTo(:,:,1));
 % my_plot([mV;mnocche]);
 
 my_skeleton(W,nocche)
-
-
-
-
-
-
 
 %%
 num_mrks=size(V(1,1).field,1);
