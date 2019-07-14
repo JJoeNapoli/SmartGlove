d44b247080a768dcf4c6c01b54bf01814c10243b
JJoeNapoli <45485426+JJoeNapoli@users.noreply.github.com>
2019-07-14 12:37:49 +0200

    Update test1.m

--- MATLAB/Kalman_code/test1.m
+++ MATLAB/Kalman_code/test1.m
@@ -1,2 +1,11 @@
-% openExample('robotics/ROSGettingStartedExample')
-a=1;
+clc
+clear
+close all
+
+rosinit('130.251.13.104')
+msgtype1 = rostopic('type','/markers_coo');
+msg1 = rosmessage(msgtype1)
+msgtype2 = rostopic('type','/Robot_1/pose');
+msg2 = rosmessage(msgtype2)
+rosshutdown
+
